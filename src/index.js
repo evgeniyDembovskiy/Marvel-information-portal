0f0184e694ce2a09af3cd0fdef3c7f0dac4c00c7
Evgeniy <dembovski4@mail.ru>
2022-12-14 15:11:49 +0300

    random character section is done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+// import MarvelService from './services/MarverService';
 import './style/style.scss';
 
+// const marvelService = new MarvelService();
+
+// marvelService.getAllCharacters().then(res => console.log(res));
+// marvelService.getCharacter(1011005).then(res => console.log(res));
 ReactDOM.render(
   <React.StrictMode>
     <App />
---
+++ src/services/MarverService.js
@@ -0,0 +1,41 @@
+
+class MarvelService {
+    _apiBase = "https://gateway.marvel.com:443/v1/public/";
+    _apiKey = "apikey=cfd29ffd189f11cbb223a7c47d1b390b"
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {  
+        let description = char.description.length === 0 ? "There is no data about the character :(" : char.description;
+        if (description.length > 220) {
+            description = description.slice(0, 217) + "...";
+        }
+        return {
+            name: char.name,
+            description: description, 
+            thumbnail: char.thumbnail.path + "." + char.thumbnail.extension, 
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        }
+    }
+}
+
+export default MarvelService;
